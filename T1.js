d96f09757d9ad8206735a4f463a7022cfc8aab10
bharathcy <bharathcy@gmail.com>
2023-03-03 22:36:40 +0530

    added js files

---
+++ T1.js
@@ -0,0 +1,57 @@
+/*
+old_age = old_age + 20
+console.log(old_age)
+
+let old_age = true
+console.log(old_age)
+
+
+let asda_100_ol8 = true
+
+
+Primitive Data Types
+	1. Can Store single value
+	2. Immutable
+	3. We can campare 
+
+Non-Primitive Data Types
+	1. Can Store multiple value
+	2. Mutable
+	3. We can not easily campare 
+
+
+
+Primitive Data Types
+-----------------------------------
+	1. Numbers ==> Integers and Float
+	2. Strings
+	3. Booleans
+	4. Null
+	5. Undefined
+	6. Symbols
+
+
+
+
+
+
+
+let width = 20
+let height = 5.7
+let area = 10
+area = width
+console.log("The area of the triangle is ", area)
+
+*/
+
+let st = "Our Nation is"
+let cy = "India"
+let current_year =  2025
+
+let width = 20
+let height = 5.7
+
+
+//Template Strings
+let out = `The area of rectange with height ${height}, width ${width} is ${width*height}`
+console.log("The output is ", out)
